b6faf3e8268a0ef18fc19016654872a120d73509
Bartek Bugała <bartek.bugala@gmail.com>
2019-06-25 21:48:40 +0200

    modified:   Kodilla-18.6/index.html 	modified:   Kodilla-18.6/script.js

--- Kodilla-18.6/script.js
+++ Kodilla-18.6/script.js
@@ -1,7 +1,10 @@
-var Counter = React.createClass({
+const ce = React.createElement;
+
+const Counter = React.createClass({
     getInitialState: function () {
         return {
-            counter: 0
+            counter: 0,
+            counter2: 1
         }
     },
 
@@ -11,16 +14,52 @@ var Counter = React.createClass({
         })
     },
 
+    decrement: function () {
+        this.setState({
+            counter: this.state.counter - 1
+        })
+    },
+    multiply: function () {
+        this.setState({
+            counter2: this.state.counter2 * 2
+        })
+    },
+
+    divide: function () {
+        this.setState({
+            counter2: this.state.counter2 / 2
+        })
+    },
+    reset: function () {
+        this.setState({
+            counter: this.state.counter = 0,
+            counter2: this.state.counter2 = 1
+        })
+    },
+
     render: function () {
-        return React.createElement('div', {onClick: this.increment },
-            React.createElement('span', {}, 'Licznik: ' + this.state.counter)
+        const style = {
+            margin: '5px'
+        }
+        return ce('div', {},
+            ce('div', {},
+                ce('span', {}, 'Licznik +/-: ' + this.state.counter),
+                ce('button', { onClick: this.increment, style: style }, '+1'),
+                ce('button', { onClick: this.decrement, style: style }, '-1')),
+            ce('div', {},
+                ce('span', {}, 'Licznik *//: ' + this.state.counter2),
+                ce('button', { onClick: this.multiply, style: style }, '*2'),
+                ce('button', { onClick: this.divide, style: style }, '/2')),
+            ce('button', { onClick: this.reset }, 'Reset')
 
         );
     }
 })
 
+const element = ce(Counter);
+const element2 = ce(Counter);
 
-
-var element = React.createElement(Counter);
+const ManyCounters = React.createClass
 
 ReactDOM.render(element, document.getElementById('app'));
+ReactDOM.render(element2, document.getElementById('app2'));
