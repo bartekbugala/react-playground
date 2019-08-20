21dd2f830cfe3e606d2ef007c18ca1f7948eaba1
Bartek Bugała <bartek.bugala@gmail.com>
2019-08-21 00:18:51 +0200

    edit script

--- Kodilla-18.2-cwiczenie/script.js
+++ Kodilla-18.2-cwiczenie/script.js
@@ -1,2 +1,19 @@
-var element = React.createElement('div', {}, 'Hello world!');
-ReactDOM.render(element,document.getElementById('app'));
+var element =
+    React.createElement('div', {},
+        React.createElement('ul', {},
+            React.createElement('li', {},
+                React.createElement('p', {}, 'Point1')
+            ),
+            React.createElement('li', {},
+                React.createElement('p', {}, 'Point2')
+            ),
+            React.createElement('li', {},
+                React.createElement('p', {}, 'Point3')
+            ),
+            React.createElement('li', {},
+                React.createElement('p', {}, 'Point4')
+            )
+        )
+    );
+
+ReactDOM.render(element, document.getElementById('app'));
